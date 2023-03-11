8121406bd2fe59dcb006a0dc1c228cc87e8583e2
IT22359988 <127512535+IT22359988@users.noreply.github.com>
2023-03-11 09:16:14 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,23 @@
 #include <stdio.h>
 
 int main() {
-  
-  return 0;
+   int mark1,mark2;
+   float average;
+   printf("Enter the first mark");
+   scanf("%d",&mark1);
+   printf("Enter the second mark");
+   scanf("%d",&mark2);
+    avg= (float)(mark1+mark2)/2;
+       //%.2f is used for displaying output upto two decimal places
+
+       printf("Average of %d and %d is: %.2f",mark1,mark2,avg);
+   return 0;
 }
+
+   
+   
+
+ 
+         
+  
 
