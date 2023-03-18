89188f9abc45717388c25b1c4d2b1f4c788db467
IT22359988 <127512535+IT22359988@users.noreply.github.com>
2023-03-18 07:01:06 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@GenuinePowerlessCircles/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+  float amount, discount, topaid;
+  
+  
+      if (amount>10000);
+      {
+        discount=(amount*25)/100;
+      topaid=amount-discount;
+}
+if(amount>1000 && amount<5000)
+{
+  discount=(amount*15)/100;
+  topaid =amount-discount;
+}
+ if(amount>5000 && amount<3000)
+{
+  discount=(amount*10)/100;
+  topaid=amount-discount;
+}
+
+                                                  
